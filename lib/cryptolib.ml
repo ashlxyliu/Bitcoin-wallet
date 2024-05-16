dfd054938d8ee71354448ddafdcd768e2cf28e30
Eliza Konvicka <ejk236@cornell.edu>
2024-05-16 09:53:00 -0400

    Eliza Konvicka (ejk236): worked on cryptolib.ml

--- lib/cryptolib.ml
+++ lib/cryptolib.ml
@@ -1,48 +1,112 @@
 open Cryptokit
 
+(* Function to compute the SHA-256 hash of a string *)
 let sha256 str =
+  (* Create a new SHA-256 hash context *)
   let hash = Hash.sha256 () in
+  (* Add the input string to the hash context *)
   hash#add_string str;
-  transform_string (Hexa.encode ()) hash#result
+  (* Compute the hash result and encode it in hexadecimal format *)
+  let result = hash#result in
+  let hex_result = transform_string (Hexa.encode ()) result in
+  (* Return the hexadecimal representation of the hash *)
+  hex_result
 
+(* Function to compute the RIPEMD-160 hash of a string *)
 let ripemd160 input =
+  (* Create a new RIPEMD-160 hash context *)
   let hash = Hash.ripemd160 () in
+  (* Add the input string to the hash context *)
   hash#add_string input;
-  transform_string (Hexa.encode ()) hash#result
+  (* Compute the hash result and encode it in hexadecimal format *)
+  let result = hash#result in
+  let hex_result = transform_string (Hexa.encode ()) result in
+  (* Return the hexadecimal representation of the hash *)
+  hex_result
 
+(* Function to convert a string to a list of characters *)
 let list_of_string str =
+  (* Recursive auxiliary function to build the list of characters *)
   let rec aux i acc = if i < 0 then acc else aux (i - 1) (str.[i] :: acc) in
+  (* Start the recursion from the end of the string *)
   aux (String.length str - 1) []
 
+(* Define the Base58 alphabet as a list of characters *)
 let base58alphabet =
   let str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz" in
   list_of_string str
 
-let hex_to_bigint hex_str = Z.of_string_base 16 hex_str
+(* Function to convert a hexadecimal string to a big integer *)
+let hex_to_bigint hex_str =
+  (* Use the Z library to parse the hexadecimal string *)
+  Z.of_string_base 16 hex_str
 
+(* Function to encode a hexadecimal string in Base58 *)
 let base58 input =
+  (* Initialize the number of leading zeroes *)
   let num_zeroes = ref 0 in
+  (* Initialize the index *)
   let i = ref 0 in
-  (* Handle leading zeroes *)
+  (* Handle leading zeroes in the input string *)
   while !i < String.length input && input.[!i] = '0' && input.[!i + 1] = '0' do
+    (* Increment the number of leading zeroes *)
     num_zeroes := !num_zeroes + 1;
+    (* Move to the next character *)
     i := !i + 2
   done;
 
-  let num = hex_to_bigint (String.sub input !i (String.length input - !i)) in
+  (* Convert the remaining part of the input string to a big integer *)
+  let hex_part = String.sub input !i (String.length input - !i) in
+  let num = hex_to_bigint hex_part in
+  (* Convert the Base58 alphabet to an array *)
   let alphabet = Array.of_list base58alphabet in
+  (* Recursive function to convert the big integer to Base58 *)
   let rec convert_to_base58 num acc =
     if Z.equal num Z.zero then acc
     else
+      (* Compute the remainder and the next number *)
       let remainder = Z.to_int (Z.rem num (Z.of_int 58)) in
       let next_num = Z.div num (Z.of_int 58) in
+      (* Add the corresponding character to the accumulator *)
       convert_to_base58 next_num (alphabet.(remainder) :: acc)
   in
+  (* Convert the big integer to Base58 *)
   let chars = convert_to_base58 num [] in
-  String.concat "" (List.map (String.make 1) chars)
+  (* Concatenate the characters into a string *)
+  let base58_str = String.concat "" (List.map (String.make 1) chars) in
+  (* Add leading '1's for each leading zero in the original input *)
+  let leading_ones = String.make !num_zeroes '1' in
+  (* Return the Base58-encoded string with leading '1's *)
+  leading_ones ^ base58_str
 
+(* Function to compute the Base58Check encoding of a hexadecimal string *)
 let base58check input =
+  (* Add a version byte (0x00 for Bitcoin) to the input string *)
   let str = "00" ^ input in
-  let checksum = String.sub (sha256 (sha256 str)) 0 4 in
-  let next_str = checksum ^ str in
-  base58 next_str
+  (* Compute the double SHA-256 hash of the string *)
+  let double_hash = sha256 (sha256 str) in
+  (* Take the first 4 bytes of the double hash as the checksum *)
+  let checksum = String.sub double_hash 0 8 in
+  (* Append the checksum to the input string *)
+  let next_str = str ^ checksum in
+  (* Encode the result in Base58 *)
+  let base58_result = base58 next_str in
+  (* Return the Base58Check-encoded string *)
+  base58_result
+
+(* Additional helper functions and steps can be added here as needed *)
+
+(* Example usage of the functions *)
+let () =
+  let input_str = "hello" in
+  let sha256_result = sha256 input_str in
+  Printf.printf "SHA-256: %s\n" sha256_result;
+
+  let ripemd160_result = ripemd160 sha256_result in
+  Printf.printf "RIPEMD-160: %s\n" ripemd160_result;
+
+  let base58_result = base58 ripemd160_result in
+  Printf.printf "Base58: %s\n" base58_result;
+
+  let base58check_result = base58check ripemd160_result in
+  Printf.printf "Base58Check: %s\n" base58check_result
