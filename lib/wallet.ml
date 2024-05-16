3c2a7ce87ddd53b66a3c23c7518a37c74230ec97
Eliza Konvicka <ejk236@cornell.edu>
2024-05-16 09:55:01 -0400

    ejk236: additional wallet logic

--- lib/wallet.ml
+++ lib/wallet.ml
@@ -1,5 +1,20 @@
+(* Function to generate a private key *)
 let generate_private_key () =
+  (* Generate a 32-byte random string using a secure random number generator *)
   let random_string = Cryptokit.Random.string Cryptokit.Random.secure_rng 32 in
-  Cryptokit.transform_string (Cryptokit.Hexa.encode ()) random_string
 
-let generate_public_key privk = Secp256k1.generate_public_key privk
+  (* Encode the random string in hexadecimal format *)
+  let hex_encoded_string =
+    Cryptokit.transform_string (Cryptokit.Hexa.encode ()) random_string
+  in
+
+  (* Return the hexadecimal encoded string as the private key *)
+  hex_encoded_string
+
+(* Function to generate a public key from a private key *)
+let generate_public_key privk =
+  (* Call the Secp256k1 module's generate_public_key function with the private key *)
+  let public_key = Secp256k1.generate_public_key privk in
+
+  (* Return the generated public key *)
+  public_key
